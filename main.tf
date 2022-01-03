b874b34a4d3b4b300eb32d72bcbcc5e98883b0a7
Akhil <36275227+maverick-27@users.noreply.github.com>
2022-01-03 06:17:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,3 +25,38 @@ resource "aws_cloudtrail" "my-trail" {
 resource "aws_cloudwatch_log_group" "trails" {
   name = "trails"
 }
+
+
+
+resource "aws_cloudwatch_metric_alarm" "metric-alarms" {
+  for_each = {
+    for m in local.metrics : m.name => m
+  }
+  alarm_name        = "${each.value.name}-alarm"
+  alarm_description = "metric from cloudtrail"
+  metric_name       = each.value.name
+  namespace         = each.value.namespace
+
+  comparison_operator = each.value.alarm.comparison_operator
+  evaluation_periods  = each.value.alarm.evaluation_periods
+  period              = each.value.alarm.period
+  statistic           = each.value.alarm.statistic
+  threshold           = each.value.alarm.threshold
+  alarm_actions = [
+    aws_sns_topic.trail-log-metrics.arn
+  ]
+}
+
+resource "aws_cloudwatch_log_metric_filter" "trail-metrics" {
+  for_each = {
+    for m in local.metrics : m.name => m
+  }
+  name           = each.value.name
+  pattern        = each.value.pattern
+  log_group_name = aws_cloudwatch_log_group.trails.name
+  metric_transformation {
+    name      = each.value.name
+    namespace = each.value.namespace
+    value     = "1"
+  }
+}
