87ffcd1f6b627f1e8d4775862418b6eaa8e99baa
Akhil <36275227+maverick-27@users.noreply.github.com>
2022-01-03 06:16:55 +0530

    Create sns.tf

---
+++ sns.tf
@@ -0,0 +1,8 @@
+resource "aws_sns_topic" "trail-log-metrics" {
+  name = "trail-log-metrics-topic"
+}
+resource "aws_sns_topic_subscription" "email-target" {
+  topic_arn = aws_sns_topic.trail-log-metrics.arn
+  protocol  = "sms"
+  endpoint  = "+91 123123123"
+}
